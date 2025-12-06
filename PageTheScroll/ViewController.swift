3a13a7be0a5f82564a3ce6b0744d365e4f9a5971
Reiss <reiss@theapplabb.com>
2025-12-06 02:03:01 -0500

    Swift 6 updates, Add peeking scroll views and update layout logic

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -1,53 +1,71 @@
-//
-//  ViewController.swift
-//  PageTheScroll
-//
-//  Created by Reiss Zurbyk on 2017-01-17.
-//  Copyright © 2017 Reiss Zurbyk. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UIScrollViewDelegate {
+
     @IBOutlet weak var scrollView: UIScrollView!
-    
-    var images = [UIImageView]()
+    private var images = [UIImageView]()
+    private var didLayout = false
+
+    // Amount of the next/previous page to show on each side
+    private let peekAmount: CGFloat = 40
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        scrollView.delegate = self
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.clipsToBounds = false  // IMPORTANT: allow peeking
 
-    override func viewDidAppear(_ animated: Bool) {
-        var contentWidth: CGFloat = 0.0
-        print("Scrollview width: \(scrollView.frame.size.width)")
-        
-        let scrollWidth = scrollView.frame.size.width
+        // Create the image views only once
         for x in 0...2 {
-            let image = UIImage(named: "icon\(x).png")
-            let imageView = UIImageView(image: image)
+            let imageView = UIImageView(image: UIImage(named: "icon\(x).png"))
+            imageView.contentMode = .scaleAspectFit
             images.append(imageView)
-            
-            var newX: CGFloat = 0.0
-            
-            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
-            contentWidth += newX
-            
             scrollView.addSubview(imageView)
-            
-            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
-        
-        scrollView.clipsToBounds = false
-        
-        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
-}
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
+        guard !didLayout else { return }
+        didLayout = true
+
+        let scrollWidth = scrollView.frame.width
+        let scrollHeight = scrollView.frame.height
+
+        // Make each page smaller than the scroll view width
+        let pageWidth = scrollWidth - (peekAmount * 2)
+        let pageHeight = scrollHeight
+
+        for (index, imgView) in images.enumerated() {
+            let xPos = CGFloat(index) * pageWidth + peekAmount * CGFloat(index + 1)
+
+            imgView.frame = CGRect(
+                x: xPos + (pageWidth - 150) / 2,
+                y: (pageHeight - 150) / 2,
+                width: 150,
+                height: 150
+            )
+        }
+
+        // Correct content size
+        scrollView.contentSize = CGSize(
+            width: CGFloat(images.count) * (pageWidth + peekAmount),
+            height: pageHeight
+        )
+    }
+
+    // Optional: snapping effect to nearest page
+    func scrollViewWillEndDragging(
+        _ scrollView: UIScrollView,
+        withVelocity velocity: CGPoint,
+        targetContentOffset: UnsafeMutablePointer<CGPoint>
+    ) {
+        let pageWidth = scrollView.frame.width - (peekAmount * 2)
+        let rawPage = targetContentOffset.pointee.x / (pageWidth + peekAmount)
+        let page = round(rawPage)
+        let newOffset = page * (pageWidth + peekAmount)
+        targetContentOffset.pointee.x = newOffset
+    }
+}
